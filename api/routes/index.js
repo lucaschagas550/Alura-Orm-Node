4efd4383683e3fbc9fb8eb725ac9eaf5fd61dd01
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2023-09-07 14:56:14 -0300

    feat: controller de pessoa e matricula

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,12 @@
 const bodyParser = require("body-parser"); // converterá os dados que chegarão no corpo, no body das requisições
 const pessoas = require("./pessoasRoute.js");
+const niveis = require("./niveisRoute.js");
+const turmas = require("./turmasRoute.js");
 
 module.exports = (app) => {
   app.use(bodyParser.json());
   app.use(pessoas);
+  app.use(niveis);
+  app.use(turmas);
   app.get("/", (req, res) => res.send("ola"));
 };
